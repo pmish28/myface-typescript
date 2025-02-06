d27ab90ad3000d6f1dcefe829ec53330fda745e5
Faith Martin <faith.martin@softwire.com>
2025-02-06 13:54:49 +0000

    Button added that uses JS to change post colours

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", function () {
-  // Add your JavaScript here.
+// document.addEventListener("DOMContentLoaded", function () {
+// // Add your JavaScript here
+// });
+
+document.getElementById("changeColourButton").addEventListener("click", () =>{
+  let items = document.getElementsByClassName("postList");
+  for (i = 0; i < items.length; i++) {
+    items[i].style.background = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`
+  }
 });
